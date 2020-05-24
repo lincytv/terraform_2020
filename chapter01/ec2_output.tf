02c2382e1a91934716a847db2c5f12f386efe375
zapcom technologies <lincytv@gmail.com>
2020-05-24 16:59:53 +0530

    session 1

---
+++ chapter01/ec2_output.tf
@@ -0,0 +1,11 @@
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+resource "aws_instance" "web_frontend_output" {
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "web_frontend"
+  }
+}
