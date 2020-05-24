659ff00e7afab52f11afa37fcee616b16a76bfd9
zapcom technologies <lincytv@gmail.com>
2020-05-24 23:34:10 +0530

    ec2-withoutput

---
+++ session2/ec2_output.tf
@@ -0,0 +1,17 @@
+
+resource "aws_instance" "web_frontend_output" {
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  iam_instance_profile = "aws-tagger-role"
+  key_name = "ssm-test"
+  tags = {
+    Name = "web_frontend"
+  }
+
+}
+output  "publicip" {
+  value = aws_instance.web_frontend_output.public_ip
+}
+output  "instance_id" {
+  value = aws_instance.web_frontend_output.id
+}
