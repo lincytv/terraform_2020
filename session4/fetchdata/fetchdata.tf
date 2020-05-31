ca5b9e7fe735be1d2b1151c34d2b7b812c324ddc
zapcom technologies <lincytv@gmail.com>
2020-05-31 08:04:46 +0530

    Add files via upload

---
+++ session4/fetchdata/fetchdata.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "frontend-web" {
+    ami = var.instance_ami_us_east_1
+    instance_type = var.inst_type["us-east-1"]
+}
+
+variable "list" {
+  type = list
+  default = ["t2.micro", "t2.small", "t2.large"]
+}
+
+variable "inst_type" {
+  type =  map
+  default = {
+    us-east-1 = "t2.micro"
+    us-west-2 = "t2.nano"
+    ap-southeast-1 = "t2.small"
+  }
+}
